0eeeec730e23176ab759228c94203505c3436c50
barakolshe <barakolshe@gmail.com>
2023-09-15 08:49:25 +0000

    added a test for book list component

---
+++ src/__mocks__/books.ts
@@ -0,0 +1,49 @@
+const booksMock: Book[] = [
+  {
+    volumeInfo: {
+      title: "Cyber War Versus Cyber Realities",
+      imageLinks: {
+        thumbnail:
+          "http://books.google.com/books/content?id=TpmeBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+    },
+  },
+  {
+    volumeInfo: {
+      title: "Cyber War Versus Cyber Realities",
+      imageLinks: {
+        thumbnail:
+          "http://books.google.com/books/content?id=TpmeBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+    },
+  },
+  {
+    volumeInfo: {
+      title: "Cyber War Versus Cyber Realities",
+      imageLinks: {
+        thumbnail:
+          "http://books.google.com/books/content?id=TpmeBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+    },
+  },
+  {
+    volumeInfo: {
+      title: "Cyber War Versus Cyber Realities",
+      imageLinks: {
+        thumbnail:
+          "http://books.google.com/books/content?id=TpmeBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+    },
+  },
+  {
+    volumeInfo: {
+      title: "Cyber War Versus Cyber Realities",
+      imageLinks: {
+        thumbnail:
+          "http://books.google.com/books/content?id=TpmeBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+    },
+  },
+];
+
+export default booksMock;
---
+++ src/pages/BooksPage/__tests__/BooksList.test.tsx
@@ -0,0 +1,24 @@
+import { expect, test } from "@jest/globals";
+import renderer from "react-test-renderer";
+import booksMock from "../../../__mocks__/books";
+import BooksList from "../BooksList/BooksList";
+
+test("BooksList check", () => {
+  let component = renderer.create(
+    <BooksList isError={true} isLoading={false} books={[]} />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  component = renderer.create(
+    <BooksList isError={false} isLoading={true} books={[]} />
+  );
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  component = renderer.create(
+    <BooksList isError={false} isLoading={false} books={booksMock} />
+  );
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
